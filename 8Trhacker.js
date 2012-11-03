5c1324e2cd11d84b17bf53e498d6117f5e796099
lukesed <lukesedney+bitbucket@gmail.com>
2012-11-03 00:09:36 -0700

    button action

--- 8Trhacker.js
+++ 8Trhacker.js
@@ -1,7 +1,21 @@
-
-$(document).ready(function() {
-  $("a").click(function() {
-    event.preventDefault()
-    alert("Hello world!");
-  });
-});
+$(document).ready(
+  function() {
+    insert_button()
+    /*
+    $("a").click(function() {
+      event.preventDefault()
+      alert("Hello world!");
+    });
+    */
+    $("#spotify_button").click(
+      function(){
+        alert("Hello world!");
+      }     
+    );
+  }
+);
+function insert_button(){
+  var icon_address = chrome.extension.getURL("icon.png");
+  $("#mix_interactions").append(
+    '<img id="spotify_button" src='+ icon_address +' alt="some_text">')
+}
