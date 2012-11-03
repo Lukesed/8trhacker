dd4726d3d2fa41aa5738fd65f1ef4e81839c4238
lukesed <lukesedney+bitbucket@gmail.com>
2012-11-03 02:43:39 -0700

    track uri done

--- 8Trhacker.js
+++ 8Trhacker.js
@@ -3,24 +3,37 @@ var songs = {}
 $(document).ready(
   function() {
     insert_button()
-    setInterval(function(){alert("scraping");scrapePage();},7000);
+    //(function(){alert("scraping");scrapePage();},7000);
     $("#spotify_button").click(
       function(){
+        uri = track_uri("fly", "fighters")
+        alert(uri)
+        console.log("uri: " + uri)
         //stuff to do when clicked button
       }     
-    );
+      );
   }
-);
+  );
+
 
 function track_uri(track, artist){
-  search_url = URLEncode('http://ws.spotify.com/search/1/track.json?q=track:"'
-              +track+'"+artist:"' +artist+'"');
-  result_json = getJSON(search_url);
-  if(!result_json.data[0]){
-    return "";
-  } else{
-    result_json["track"][0]["href"].split(":")[2];
-  }
+  search_url = encodeURI('http://ws.spotify.com/search/1/track.json?q=track:'
+    +track+'+artist:' +artist);
+  alert(search_url)
+  var uri = ""
+  $.ajax({
+    url: search_url,
+    async: false,
+    dataType: 'json',
+    success:
+    function(json){
+      console.log(json["tracks"])
+      uri = json["tracks"][0]["href"].split(":")[2]
+      console.log(uri)
+    } 
+  });
+  console.log("2: " + uri)
+  return uri;
 }
 
 
