f6db9b01186f9de860754f498537b8b58d5e5ef6
Dominick Reinhold <d.reinhold@yahoo.com>
2012-11-03 01:21:17 -0700

    fixed page scraper

--- 8Trhacker.js
+++ 8Trhacker.js
@@ -1,8 +1,9 @@
+var songs = {}
+
 $(document).ready(
   function() {
     insert_button()
-    var songs = []
-    setInterval(scrapePage(songs),5000);
+    setInterval(function(){alert("scraping");scrapePage();},7000);
     $("#spotify_button").click(
       function(){
         //stuff to do when clicked button
@@ -17,12 +18,13 @@ function insert_button(){
     '<img id="spotify_button" src='+ icon_address +' alt="some_text">')
 }
 
-function scrapePage(songs){
-  alert("scraping page")
-  $(".title_artist").each(function(){
-   // alert(idx)
-   // if (idx >= songs.length){
+function scrapePage(){
+  alert(songsToString())
+  $("#tracks_played .track .title_artist").each(function(idx){
+    if (!(idx in songs)){
+      alert("adding song!")
       var song = {}
+      
       $(this).children().each(function(index){
         if (index == 0){
           song["artist"] = $(this).text()
@@ -30,18 +32,18 @@ function scrapePage(songs){
           song["title"] = $(this).text()
         }
       });
+      song["uri"] = track_uri(song["title"],song["artist"])
       //alert(song["artist"]+": "+song["title"])
-      songs.push(song)
-    //}
+      songs[idx] = song
+    }
   });  
-  alert(songsToString(songs))
-  //return songs  
+  alert(songsToString())
 };
 
-function songsToString(songs){
+function songsToString(){
   s = ""
-  for(i=0;i<songs.length;i++){
-    s=s+songs[i]["artist"]+": "+songs[i]["title"]+"\n"
+  for(i=0;i<Object.keys(songs).length;i++){
+    s=s+i+": "+songs[i]["artist"]+": "+songs[i]["title"]+"\n"
   }
   return s
 }
