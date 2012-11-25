46288082e7387d5c10b8db3d2f6e8b0b4d7705c5
Dominick Reinhold <d.reinhold@yahoo.com>
2012-11-25 13:21:07 -0800

    Added GA code

--- 8Trhacker.js
+++ 8Trhacker.js
@@ -1,5 +1,16 @@
 $(document).ready(
   function() {
+    // set up GA
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-36582718-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = 'https://ssl.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
     // Initialize our empty songs container
     var songs = {};
     // Insert the new buttons into the 8tracks page
